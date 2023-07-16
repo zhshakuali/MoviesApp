9202c962164522b97ea301a82d6c2cb09088ba9c
zhshakuali <zhansayashakuali200309@gmail.com>
2023-07-16 11:59:37 +0600

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Жансая Шакуали on 16.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
